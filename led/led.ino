126b4ab65ce8d458602fcb47c79160d7c32607ab
vissionvesel <heartengineer1999@gmail.com>
2020-11-23 19:54:33 +0530

    updated

--- led/led.ino
+++ led/led.ino
@@ -1,1 +1,19 @@
+int dtime=1000;
+int pin=8,pin2=5;
 
+void setup() {
+  pinMode(pin,OUTPUT);
+  delay (dtime);
+  pinMode(pin2,OUTPUT);
+  
+  
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+   digitalWrite(OUTPUT,HIGH);
+  // put your main code here, to run repeatedly:
+  delay(1000);
+
+}
